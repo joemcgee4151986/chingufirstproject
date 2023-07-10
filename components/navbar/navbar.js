a6e02f9e14da540d6b1963b51fe6880ee18a66f8
joemcgee4151986 <joerobertmcgee@gmail.com>
2023-07-10 10:51:03 +0900

    redesigned navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -7,54 +7,67 @@ class navbar extends HTMLElement {
       this.innerHTML = `
       <navbar>
       <style>
-      nav{
-        position: sticky;
+      nav {
+        height: 40px;
         display: flex;
-  align-items: center;
+        align-items: center;
+        justify-content: center;
+        background-color:  #333;
       }
-ul{
-  overflow: hidden;
-  ext-decoration: none;
-  color:white;
-  background-color: #383B3C;
-}
-       
-li{
-  float: left;
+      ul li {
+        list-style: none;
+        display: inline;
+      }
+      a {
+        text-decoration: none;
+        color: white;
+      }
+      .btn {
+        background-color:  #333;
+        color: white;
+        border: 1px solid gray;
+        padding: 6px 28px;
+      }
+      .btn:hover {
+        background-color: #49505F;
+        
+      }
+.burger-menu {
+  color: var(--primary-color);
+  font-size: 2rem;
+  border: 0;
+  background-color: transparent;
+  cursor: pointer;
+  display: none;
 }
 
-li a {
-  display: block;
-  padding: 8px;
-}
-navbar{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-@media screen and (max-width: 600px) {
-    .column {
-    width: 100%;
-    }
+@media only screen and (max-width: 800px) {
+  header nav {
+    
   }
+      </navbar>
         </style>
+        <header>
         <nav>
-        <div class="topnav">
-        <ul style="list-style-type:none;">
-            <li><a href = "#">Homepage</a></li>
-            <li><a href = "#">About Us</a></li>
-            <li><a href = "#">Services</a></li>
-            <li><a href = "#">Contact</a></li>
-            <li><a href = "#">External </a></li>
+        <ul id ="navbar">
+          <li>
+            <a href="#" button class = "btn">Home</a>
+          </li>
+          <li>
+            <a href="#" button class = "btn">About</a>
+          </li>
+          <li>
+            <a href="#" button class = "btn">Services</a>
+          </li>
+          <li>
+          <a href="#" button class = "btn">Contact</a>
+        </li>
+        <li>
+          <a href="#" button class = "btn">External</a>
+        </li>
         </ul>
-        <button class="burger-menu" id="burger-menu">
-        <ion-icon class="bars" name="menu-outline"></ion-icon>
-        </button>
-        <section = section1>
-        <header id = header></header>
-        <img src = "https://images.unsplash.com/photo-1688408958776-00a5c1b17c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80">
-    </div>
-    </nav>
+        </nav>
+        </header>
       `;
     }
   }
